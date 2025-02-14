a46843e649495ec28f7139d2db965a2bac90c8db
Ivan Belov <dionis.krasava@gmail.com>
2025-02-14 17:13:02 +0700

    Адаптативное расширение столба таблицы и иные мелкие изменения

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -19,11 +18,11 @@ func main() {
 	defer db.Close()
 
 	a := app.New()
-	w := a.NewWindow("Управление словарем")
-	w.Resize(fyne.NewSize(500, 200))
+	w := a.NewWindow("Английские слова")
+	w.Resize(fyne.NewSize(300, 200))
 
 	// Устанавливаем светлую тему
-	a.Settings().SetTheme(theme.LightTheme())
+	//a.Settings().SetTheme(theme.LightTheme())
 
 	// Элементы интерфейса
 	lab1 := widget.NewLabelWithStyle("Добавьте новое слово", fyne.TextAlignCenter, fyne.TextStyle{Bold: true})
@@ -34,75 +33,14 @@ func main() {
 
 	// Кнопки
 	btn1 := widget.NewButton("Добавить", func() { addDB(db, wordEntry, translationEntry) })
-	btn2 := widget.NewButton("Удалить", func() {})
-	btn3 := widget.NewButton("Редактировать", func() {
-		// Выполняем запрос для получения данных из таблицы
-		rows, err := db.Query("SELECT id, word, translation FROM words")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer rows.Close()
-
-		// Создаем слайс для хранения данных
-		var data [][]string
-
-		// Читаем данные из результата запроса
-		for rows.Next() {
-			var id int
-			var word string
-			var translation string
-			err = rows.Scan(&id, &word, &translation)
-			if err != nil {
-				log.Fatal(err)
-			}
-			data = append(data, []string{fmt.Sprint(id), word, translation})
-		}
-
-		// Проверяем на ошибки после чтения
-		if err = rows.Err(); err != nil {
-			log.Fatal(err)
-		}
-
-		// Создаем новое окно
-		newWindow := a.NewWindow("Окно редактирования")
-		newWindow.Resize(fyne.NewSize(600, 400))
-
-		// Если данных нет, выводим сообщение
-		if len(data) == 0 {
-			newWindow.SetContent(widget.NewLabel("Нет данных для отображения"))
-			newWindow.Show()
-			return
-		}
-
-		// Создаем таблицу для отображения данных
-		table := widget.NewTable(
-			func() (int, int) {
-				return len(data), len(data[0])
-			},
-			func() fyne.CanvasObject {
-				return widget.NewLabel("template")
-			},
-			func(i widget.TableCellID, o fyne.CanvasObject) {
-				o.(*widget.Label).SetText(data[i.Row][i.Col])
-			},
-		)
-
-		// Кнопка для закрытия окна
-		closeBtn := widget.NewButton("Закрыть", func() {
-			newWindow.Close()
-		})
-
-		// Устанавливаем содержимое нового окна
-		newWindow.SetContent(container.NewBorder(nil, closeBtn, nil, nil, container.NewScroll(table)))
-		newWindow.Show()
-	})
+	btn3 := widget.NewButton("Посмотреть", func() { showWords(a, db) })
 
 	// Компоновка интерфейса
 	rows1 := container.NewGridWithColumns(1, wordEntry)
 	rows2 := container.NewGridWithColumns(1, translationEntry)
-	rows3 := container.NewGridWithColumns(3, btn1, btn2, btn3) // строка кнопок
+	rows3 := container.NewGridWithColumns(2, btn1, btn3) // строка кнопок
 
-	rowsCont := container.NewGridWithRows(5, lab1, rows1, rows2, rows3)
+	rowsCont := container.NewGridWithRows(4, lab1, rows1, rows2, rows3)
 	w.SetContent(rowsCont)
 
 	w.ShowAndRun()
@@ -148,4 +86,91 @@ func addDB(db *sql.DB, wordEntry *widget.Entry, translationEntry *widget.Entry) 
 	// Очистка полей ввода
 	wordEntry.SetText("")
 	translationEntry.SetText("")
+}
+
+func showWords(a fyne.App, db *sql.DB) {
+	// Выполняем запрос для получения данных из таблицы
+	rows, err := db.Query("SELECT id, word, translation FROM words")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	// Создаем слайс для хранения данных
+	var data [][]string
+
+	// Читаем данные из результата запроса
+	for rows.Next() {
+		var id int
+		var word string
+		var translation string
+		err = rows.Scan(&id, &word, &translation)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data = append(data, []string{fmt.Sprint(id), word, translation})
+	}
+
+	// Проверяем на ошибки после чтения
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Создаем новое окно
+	newWindow := a.NewWindow("Просмотр слов")
+	newWindow.Resize(fyne.NewSize(600, 400))
+
+	// Если данных нет, выводим сообщение
+	if len(data) == 0 {
+		newWindow.SetContent(widget.NewLabel("Нет данных для отображения"))
+		newWindow.Show()
+		return
+	}
+
+	//----------------------------------------------
+	// Функция для вычисления максимальной ширины текста в столбце
+	calculateMaxColumnWidth := func(columnIndex int) float32 {
+		maxWidth := float32(0)
+		for _, row := range data {
+			text := row[columnIndex]
+			// Создаем временный Label для измерения ширины текста
+			label := widget.NewLabel(text)
+			label.Refresh() // Обновляем, чтобы рассчитать размер
+			width := label.MinSize().Width
+			if width > maxWidth {
+				maxWidth = width
+			}
+		}
+		return maxWidth
+	}
+
+	// Вычисляем максимальную ширину для второго столбца (слово)
+	maxWordWidth := calculateMaxColumnWidth(1)
+
+	//----------------------------------------------
+
+	// Создаем таблицу для отображения данных
+	table := widget.NewTable(
+		func() (int, int) {
+			return len(data), len(data[0])
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("template")
+		},
+		func(i widget.TableCellID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(data[i.Row][i.Col])
+		},
+	)
+
+	// Устанавливаем ширину второго столбца на основе максимальной ширины текста
+	table.SetColumnWidth(1, maxWordWidth+20) // Добавляем небольшой отступ (20 пикселей)
+
+	// Кнопка для закрытия окна
+	closeBtn := widget.NewButton("Закрыть", func() {
+		newWindow.Close()
+	})
+
+	// Устанавливаем содержимое нового окна
+	newWindow.SetContent(container.NewBorder(nil, closeBtn, nil, nil, container.NewScroll(table)))
+	newWindow.Show()
 }
